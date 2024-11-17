80cbd4ae0b8d127f23642b1d2124db37119c71e2
subharthi75 <subharthi75@gmail.com>
2024-11-17 22:41:15 +0530

    srete

--- app/components/dashboard/RecentSales.tsx
+++ app/components/dashboard/RecentSales.tsx
@@ -47,7 +47,7 @@ export async function RecentSales() {
               </p>
             </div>
             <p className="ml-auto font-medium">
-              +${new Intl.NumberFormat("en-US").format(item.amount / 100)}
+              +${new Intl.NumberFormat("en-US").format(item.amount)}
             </p>
           </div>
         ))}
